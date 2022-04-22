f4ec1c51894883788cf8be967f2396bfba3136f4
alanli <alan12141020@gmail.com>
2022-04-22 15:11:16 +0800

    Setup icon component

---
+++ src/components/IconBase.vue
@@ -0,0 +1,44 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="height"
+    viewBox="0 0 24 24"
+    :aria-labelledby="iconName"
+    role="presentation"
+  >
+    <g :fill="iconColor">
+      <slot />
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    iconName: {
+      type: String,
+      default: "",
+    },
+    width: {
+      type: [Number, String],
+      default: 24,
+    },
+    height: {
+      type: [Number, String],
+      default: 24,
+    },
+    iconColor: {
+      type: String,
+      default: "#FFFFFF",
+    },
+  },
+};
+</script>
+
+<style scoped>
+svg {
+  display: inline-block;
+  vertical-align: baseline;
+}
+</style>
