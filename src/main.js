b96452f49f4ba2d76b6617d14abe598342dffd9f
alanli <alan12141020@gmail.com>
2022-04-22 15:10:45 +0800

    Setup global scss

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import { BootstrapVue } from "bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "./app.scss";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
