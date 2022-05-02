1c4feb08f052ba2603a2357796c922a5c98774cf
alanli <alan12141020@gmail.com>
2022-05-02 12:47:37 +0800

    Add deploy script

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:lisinhong/filet.git main:gh-pages
+git push -f git@github.com:lisinhong/filet.git master:gh-pages
 
 cd -
